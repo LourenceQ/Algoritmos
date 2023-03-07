1a31d64bf1620fbd7ef8a2418770a14a24eb0518
LourenceQ <lawrenceqf@gmail.com>
2023-03-06 22:35:09 -0300

    Solução para problema MinimumTimeToCompleteTrip

---
+++ 2187MinimumTimeToCompleteTrips/MainProj/Program.cs
@@ -0,0 +1,10 @@
+namespace _2187MinimumTimeToCompleteTripsProj
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ 2187MinimumTimeToCompleteTrips/MainProj/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace _2187MinimumTimeToCompleteTripsProj;
+public static class Solution
+{
+    public static long MinimumTime(int[] time, int totalTrips)
+    {
+        if(time.Length == 1) return ((long)time[0] * totalTrips);
+
+        long startTime = time.Min();
+        long result = ((long)startTime * totalTrips);
+        long endTime = result;
+
+        while(startTime <= endTime)
+        {
+            long timeTaken = startTime + (endTime - startTime + 1) / 2;
+
+            if(time.Sum(x => timeTaken / x) >= totalTrips)
+            {
+                result = Math.Min(result, timeTaken);
+                endTime = timeTaken -1;
+            }
+            else
+                startTime = timeTaken + 1;
+        }
+
+        return result;
+    }
+}
