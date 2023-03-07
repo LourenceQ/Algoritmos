7c2867b0e1d6426b894c6caeba7efe0ea614d731
LourenceQ <lawrenceqf@gmail.com>
2023-03-06 22:01:15 -0300

    Corrigindo testes de unidade par problema 443StringCompression

--- 443StringCompressionProblem/xUnitTests/UnitTest1.cs
+++ 443StringCompressionProblem/xUnitTests/UnitTest1.cs
@@ -4,34 +4,32 @@ namespace xUnitTests;
 
 public class UnitTest1
 {
-    public static IEnumerable<object[]> GetArgs
+    public static IEnumerable<object[]> GetArgs()
     {
-        get
-        {
-            yield return new object[]
-        {
-             new char[] { 'a','a','b','b','c','c','c' }
+        yield return new object[] 
+        { 
+            new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }
+            , new char[] { 'a', '2', 'b', '2', 'c', '3' }, 6 
         };
-
-        yield return new object[]
-        {
-             new char[] { 'a' }
+        yield return new object[] 
+        { 
+            new char[] { 'a' }
+            , new char[] { 'a' }, 1 
         };
-
-        yield return new object[]
-        {
-             new char[] { 'a','b','b','b','b','b','b','b','b','b','b','b','b' }
+        yield return new object[] 
+        { 
+            new char[] { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' }
+            , new char[] { 'a', 'b', '1', '2' }, 4 
         };
-        }
+    
     }
 
     [Theory
     , MemberData(nameof(GetArgs))]
-    public void CompressTest(char[] args)
+    public void CompressTest(char[] input, char[] expectedOutput, int expectedLength)
     {
-        var ans = Solution.Compress(args);
-        Assert.Equal(6, ans);
-        Assert.Equal(1, ans);
-        Assert.Equal(4, ans);
+        var actualLength  = Solution.Compress(input);
+        Assert.Equal(expectedLength, actualLength );
+        Assert.Equal(expectedOutput, input[..actualLength ]);
     }
 }
