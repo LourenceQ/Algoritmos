45aa7507dad7dfeb229cd889c242eebc9a25d4a4
LourenceQ <lawrenceqf@gmail.com>
2024-02-28 16:57:01 -0300

    Implementando algoritmo de problema para parentheses válidos

---
+++ ValidParenthesesAlgorithm/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ValidParenthesesAlgorithm;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+    public static bool IsValid(string s)
+    {
+        Stack<char> st = new Stack<char>();
+
+
+        foreach (var item in s.ToCharArray())
+        {
+            if (item == '(')
+            {
+                st.Push(')');
+            }
+            else if (item == '[')
+            {
+                st.Push(']');
+            }
+            else if (item == '{')
+            {
+                st.Push('}');
+            }
+            else if (st.Count == 0 || st.Pop() != item)
+            {
+                return false;
+            }
+        }
+
+        return st.Count == 0;
+    }
+}
---
+++ ValidParenthesesTestSuit/ValidParenthesesUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using ValidParenthesesAlgorithm;
+using Xunit;
+
+namespace ValidParenthesesTestSuit;
+public class ValidParenthesesUnitTests
+{
+    [Theory]
+    [InlineData("{{{}}}")]
+    [InlineData("[{{}}]")]
+    public static void IsValid_Deve_Retornar_Tue(string input)
+    {
+        // Arrange & Act
+        var res = Program.IsValid(input);
+
+        // Assert
+        Assert.True(res);
+    }
+}
