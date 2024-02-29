19d29b994bc5810fa61037af6398b1bffadbf4a7
LourenceQ <lawrenceqf@gmail.com>
2024-02-29 19:38:21 -0300

    Implementando algoritmo para problema de Validar sequence de Stack

---
+++ ValidateStackSequencesAlgorithm/ValidateStackSequences.cs
@@ -0,0 +1,40 @@
+﻿namespace ValidateStackSequencesAlgorithm;
+
+public static class ValidateStackSequences
+{
+    public static bool Validate(int[] pushed, int[] popped)
+    {
+        var pushedLen = pushed.Length;
+
+        int push = 0, pop = 0;
+
+        var stack = new Stack<int>();
+
+        while (push < pushedLen || pop < pushedLen)
+        {
+            if (stack.Count == 0)
+            {
+                if (push >= pushedLen)
+                {
+                    break;
+                }
+                stack.Push(pushed[push++]);
+            }
+            else if (popped[pop] == stack.Peek())
+            {
+                stack.Pop();
+                pop++;
+            }
+            else if (push < pushedLen)
+            {
+                stack.Push(pushed[push++]);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
---
+++ ValidateStackSequencesTestSuit/ValidateStackSequencesUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using ValidateStackSequencesAlgorithm;
+using Xunit;
+
+namespace ValidateStackSequencesTestSuit;
+public class ValidateStackSequencesUnitTests
+{
+    public static IEnumerable<object[]> DataTestToBeTrue()
+    {
+        yield return new object[]
+        {
+            new int[] {1,2,3,4,5}, new int[] { 4, 5, 3, 2, 1 }
+        };
+    }
+
+    public static IEnumerable<object[]> DataTestToBeFalse()
+    {
+        yield return new object[]
+        {
+            new int[] {1,2,3,4,5}, new int[] { 4, 3, 5, 1, 2 }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(DataTestToBeTrue))]
+    public void Validate_Deve_Ser_True(int[] pushed, int[] popped)
+    {
+        // Arrange, Act
+        bool sutTrue = ValidateStackSequences.Validate(pushed, popped);
+
+        // Assert
+        sutTrue.Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(DataTestToBeFalse))]
+    public void Validate_Deve_Ser_False(int[] pushed, int[] popped)
+    {
+        // Arrange, Act
+        bool sut = ValidateStackSequences.Validate(pushed, popped);
+
+        // Assert
+        sut.Should().BeFalse();
+    }
+
+
+}
