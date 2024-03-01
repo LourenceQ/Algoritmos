ea882d654c066474b6585080b20a0c822fd8c1b2
LourenceQ <lawrenceqf@gmail.com>
2024-03-01 19:53:15 -0300

    Implementando algoritmo para problema de Two Sum

---
+++ TwoSumAlgorithm/TwoSum.cs
@@ -0,0 +1,19 @@
+﻿namespace TwoSumAlgorithm;
+public static class TwoSum
+{
+    public static int[] Validate(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+
+        return new int[2];
+    }
+}
