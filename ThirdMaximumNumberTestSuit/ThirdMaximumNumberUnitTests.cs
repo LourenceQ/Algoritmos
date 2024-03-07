1726396e3089bccb6cf0c7ea7b19a5a7ca2f31e1
LourenceQ <lawrenceqf@gmail.com>
2024-03-06 22:03:00 -0300

    implementando testes para algoritmo para problema hird Maximum Number

---
+++ ThirdMaximumNumberTestSuit/ThirdMaximumNumberUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using ThirdMaximumNumberAlgorithm;
+using Xunit;
+
+namespace ThirdMaximumNumberTestSuit;
+
+public class ThirdMaximumNumberUnitTests
+{
+    public static IEnumerable<object[]> TestData =>
+    [
+        [new int[] { 3, 2, 1 }, 1]
+    ];
+
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void ThirdMaximumNumber_Must_Be_True(int[] data, int expectedResult)
+    {
+        // Arrange, Act
+        var sut = ThirdMaximumNumber.Validate(data);
+
+        // Assert
+        sut.Should().Be(expectedResult);
+    }
+}
