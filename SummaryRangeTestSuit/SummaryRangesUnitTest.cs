9144db54ed6f634a01f34169905354e2a824e678
LourenceQ <lawrenceqf@gmail.com>
2024-03-08 20:10:37 -0300

    implementando teste para nullabilidade de método

---
+++ SummaryRangeTestSuit/SummaryRangesUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using SummaryRangesAlgorithm;
+using Xunit;
+
+namespace SummaryRangeTestSuit;
+public class SummaryRangesUnitTest
+{
+    [Fact]
+    public void SummaryRanges_Deve_Retornar_Ok()
+    {
+        // Arrange
+        var res = SummaryRanges.Validate([0, 1, 2, 4, 5, 7]);
+
+        // Act
+
+        // Assert
+        res.Should().NotBeNull();
+
+    }
+}
