e996f8f61eaf334508380998b32ddf85934e1ede
LourenceQ <lawrenceqf@gmail.com>
2024-03-11 20:20:50 -0300

    implementando algotirmo para problema SubtractTheProductAndSumOfDigitsOfAnInteger

---
+++ SubtractTheProductAndSumOfDigitsOfAnIntegerAlgotithm/SubtractTheProductAndSumOfDigitsOfAnInteger.cs
@@ -0,0 +1,28 @@
+﻿namespace SubtractTheProductAndSumOfDigitsOfAnIntegerAlgotithm;
+
+public static class SubtractTheProductAndSumOfDigitsOfAnInteger
+{
+    public static int SubtractProductAnsSum(int n)
+    {
+        int mult = 1, sum = 0;
+
+        List<int> digits = [];
+
+        if (n == 0) return 0;
+
+        while (n > 0)
+        {
+            var digit = n % 10;
+            n /= 10;
+            digits.Add(digit);
+        }
+
+        for (int x = 0; x < digits.Count; x++)
+        {
+            mult *= digits[x];
+            sum += digits[x];
+        }
+
+        return mult - sum;
+    }
+}
