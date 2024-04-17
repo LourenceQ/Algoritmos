85c16d4bfc3f7dc40419da9d24203b231b0341f9
LourenceQ <lawrenceqf@gmail.com>
2024-04-16 21:39:41 -0300

    implementando algoritmo para problema Third Maximum Number

---
+++ ThirdMaximumNumberAlgorithm/ThirdMaximumNumber.cs
@@ -0,0 +1,31 @@
+﻿namespace ThirdMaximumNumberAlgorithm;
+
+public static class ThirdMaximumNumber
+{
+    public static int Validate(int[] nums)
+    {
+        long max = nums[0], max2nd = long.MinValue, max3rd = long.MinValue;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > max)
+            {
+                max3rd = max2nd;
+                max2nd = max;
+                max = nums[i];
+            }
+            else if (nums[i] > max2nd)
+            {
+                if (nums[i] != max)
+                {
+                    max3rd = max2nd;
+                    max2nd = nums[i];
+                }
+            }
+            else if (nums[i] > max3rd)
+                if (nums[i] != max2nd)
+                    max3rd = nums[i];
+        }
+
+        return (int)(max3rd != long.MinValue ? max3rd : max);
+    }
+}
