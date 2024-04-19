b473d35034db5100380596ba0456990d9cc94eb2
LourenceQ <lawrenceqf@gmail.com>
2024-04-19 20:57:28 -0300

    Implementando testes de unidade para algoritmo split array largest sum

---
+++ SplitArrayLargestSumTestSuit/SplitArrayLargestSumUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using SplitArrayLargestSumAlgorithm;
+using Xunit;
+
+namespace SplitArrayLargestSumTestSuit;
+
+public class SplitArrayLargestSumUnitTest
+{
+    public static IEnumerable<object[]> TestData =>
+        [
+            [new int[] { 7, 2, 5, 10, 8 }, 2, 18]
+        ];
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public static void SplitArrayLargestSum_Deve_Retornar_True(int[] data, int m, int expectedResult)
+    {
+        // Arrange, Act
+        var sut = SplitArrayLargestSum.Validate(data, m);
+
+        // Assert
+        sut.Should().Be(expectedResult);
+    }
+}
